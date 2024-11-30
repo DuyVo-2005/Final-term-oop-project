ffb4978d1ec26ffeb7beed0abd8e6dc0f3c44333
Võ Lê Khánh Duy <162585498+DuyVo-2005@users.noreply.github.com>
2024-11-30 20:54:43 +0700

    Update oop cuối kỳ.py

--- oop cuối kỳ.py
+++ oop cuối kỳ.py
@@ -1,20 +1,16 @@
 from tkinter import *
 from tkinter import messagebox
 import datetime
+import random
 
+TransferID_List = []
 
 # transaction vs transfer
-class Transaction:
-    def __init__(
-        self, TransactionID="", Transaction_Type="", Amount=0.0, Time="", Note=""
-    ):
-        self.TransactionID = TransactionID
-        self.Transaction_Type = Transaction_Type
-        self.Amount = Amount
-        self.Time = Time
-        self.Note = Note
 
-    def create_transaction(self, TransactionID, Transaction_Type, Amount, Time, Note):
+class User:
+    def __init__(self):
+        self.Transaction_List = []
+    def Create_Transaction(self, TransactionID, Transaction_Type, Amount, Time, Note):
         # print("Tạo giao dịch mới")
         # self.TransactionID = input("Nhập mã giao dịch:")
         # self.transaction_type = input("Nhập loại giao dịch: ")
@@ -48,17 +44,67 @@ class Transaction:
                 "Notification", "Date time must be in format DD/MM/YYYY !"
             )
         self.Note = Note
-
-    def delete_transaction(self, TransactionID):
-        # trong user
-        pass
-
-    def edit_transaction(self, TransactionID, transaction_type, amount, time, note):
+        
+class Transaction(User):
+    def __init__(
+        self, TransactionID="", Transaction_Type="", Amount=0.0, Time="", Note=""
+    ):
+        super().__init__()
         self.TransactionID = TransactionID
-        self.transaction_type = transaction_type
-        self.time = time
-        self.note = note
+        self.Transaction_Type = Transaction_Type
+        self.Amount = Amount
+        self.Time = Time
+        self.Note = Note
 
+    def Edit_Transaction(self, TransactionID, Transaction_Type, Amount, Time, Note):
+        self.TransactionID = TransactionID
+
+        if (
+            Transaction_Type.capitalize() != "Income"
+            or Transaction_Type.capitalize() != "Spending"
+        ):
+            messagebox.showerror(
+                "Notification", "Transaction type must be Income or Spending!"
+            )
+        else:
+            self.Transaction_Type = Transaction_Type
+
+        try:
+            self.Amount = float(Amount)
+        except ValueError:
+            messagebox.showerror("Notification", "Amount must be float")
+
+        # true format for time: DD/MM/YYYY
+        Date_Format = "%Y/%m/%d %H:%M:%S"
+        try:
+            Date_Format = "%d/%m/%Y %H:%M:%S"
+            self.Time = Time
+            dt = datetime.datetime.strptime(Time, Date_Format)
+        except ValueError:
+            messagebox.showerror(
+                "Notification", "Date time must be in format DD/MM/YYYY !"
+            )
+        self.Note = Note
+
+    def GetTransactionHistory(AccountName):
+        """Function to get transaction history
+        Parameters:
+            AccountName (string): The name of account want to get transaction history
+        Returns:
+            List: array contains transaction history list     
+        """        Transaction_History = []
+        for Transaction_ in super().Transaction_List:
+            if Transction_.TransactionID == TransactionID:
+                    
+ransaction_History.append(Transaction_)
+#                print(f"Transaction ID: {Transaction_.TransactionID}")
+
+#                print(f"Transaction Type: {Transaction_.Transaction_Type}")
+
+#                print(f"Amount: {Transaction_.Amount}")
+
+    def Transaction_Classification(self):
+        pass
 
 class Transfer:
     def __init__(self):
@@ -68,5 +114,12 @@ class Transfer:
         self.Amount = 0.0
         self.Time = ""
 
-    def Make_Transfer(Destination):
-        pass
+    def Make_Transfer(Sour e_Account, Destination_Account):
+        Temporary_TransferID = str(random.randint(1,10000000000))
+        while Temporary_TransferID not in TransferID_List:
+            self.TransferID = Temporary_TransferID
+            TransferID_List.append(Temporary_TransferID)
+            self.Account_Name_Source = Soure_Account.Account_Name
+             self.Account_Name_Destination =    Destination_Account.Account_Name
+
+        
