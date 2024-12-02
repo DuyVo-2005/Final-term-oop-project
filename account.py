0ca73882ec985aeb0db8ecf43f207f3a66fa0a43
VanPhuHien <phien0689@gmail.com>
2024-12-02 18:08:58 +0700

    Update account.py

--- account.py
+++ account.py
@@ -1,83 +1,134 @@
 from tkinter import messagebox
+from trans import *
+import random
+import datetime
 
 class Account:
-    
-    def __init__(self, accountType = "", accountName = "", balance = 0):
+    def __init__(self, balance: int, accountType: str = "", accountName: str = ""):
         self.__accountType = accountType
         self.__accountName = accountName
         self.__balance = balance
         self.__transactionList = []
-        self.__loanList = []
+        self.__transferList = []
+        self.__DebtList = []
 
 
+    def __CreateID(self) -> int:
+        __listID = []
+        for Trans in self.__transactionList:
+            __listID.append(Trans.Get_ID())
+        for Loans in self.__loanList:
+            __listID.append(Loans.Get_ID())
+
+        __thisTranID = random.randint(10_000_000, 99_999_999)
+        while __thisTranID in __listID:
+            __thisTranID = random.randint(10_000_000, 99_999_999)
+
+        return __thisTranID
+    
+    def __GetPresentTime(self) -> str:
+        pass
+
+        
+        
     """Edit account information such as account type, account name and balance"""
-    def Edit_Account(self, accountType, accountName, balance):
+    def Edit_Account(self, balance: int, accountType: str, accountName: str):
         self.__accountType = accountType
         self.__accountName = accountName
         try: 
             self.__balance = int(balance)
         except ValueError:
-            messagebox.showerror("Notification","balance must be interger")
+            messagebox.showerror("Notification","Balance must be number")
+        
         
         
     """Make money transfers between internal accounts"""
-    def Transfer(self, transferID, destinationAccount, amount, time):
-        action = Transfer(transferID, self.__accountName, accountNameDestination, amount, time)        
-        action.Make_Transfer(self.__accountName, destinationAccount, amount)
+    def Create_Transfer(self, destinationAccount: str, amount: int, time: str = "DD/MM/YYYY HH:MM:SS"):
+        if time == "DD/MM/YYYY HH:MM:SS":
+            time = self.__GetPresentTime()
+            
+        newTransfer = Transfer(self.__CreateID(), self.__accountName, destinationAccount, amount, time)        
+        newTransfer.Make_Transfer(self.__accountName, destinationAccount, amount)
+        
         
     
     """Create a new transaction"""
-    def Create_Transaction(self, transactionID, transactionType, amount, time, note):
-        for transaction in self.__transactionList:
-            if transactionID == transaction.Get_ID():
-                messagebox.showerror("Notification","This transaction id already exists!")
-                return
-        newTransaction = Transaction(transactionID, transactionType, amount, time, note)
-        if transactionType == "Income":
-            self.__balance += amount
-        else:
-            self.__balance -= amount
-        self.__transactionList.append(newTransaction)
-         
-    
-    """Delete transaction via transaction ID"""
-    def Delete_Transaction(self, transactionID):
-        if len(self.__transactionList) == 0:
-            messagebox.showerror("Notification", "Transation list is empty! Can't delete!")
-        elif transactionID not in self.__transactionList:
-            messagebox.showerror("Notification", "Transaction id doesn't exist! Can't delete!")
-        else:
-            for transaction in self.__transactionList:
-                if transactionID == transaction.Get_ID():
-                    self.__transactionList.remove(transaction)
-                    messagebox.showinfo("Notification","Delete Successfully!")
-                    break
-    
-    
-    """Delete transaction via transaction ID"""
-    def Update_Transaction(self, transactionID):
-        if len(self.__transactionList) == 0:
-            messagebox.showerror("Notification", "Transation list is empty! Can't update!")
-        elif transactionID not in self.__transactionList:
-            messagebox.showerror("Notification", "Transaction id doesn't exist! Can't update!")
-        else:
-            for transaction in self.__transactionList:
-                if transactionID == transaction.Get_ID():
-                    transaction.Edit_Transaction(transactionType, amount, time, note, catalog)
-                    messagebox.showinfo("Notification","Update Successfully!")
-                    break
+    def Create_Transaction(self, transactionType: str, amount: int, time: str = "DD/MM/YYYY HH:MM:SS", note: str = ""):
+        if time == "DD/MM/YYYY HH:MM:SS":
+            time = self.__GetPresentTime()
         
+        newTransaction = Transaction(self.__CreateID(), transactionType, amount, time, note)
+        self.__balance += newTransaction.Get_Amount()
+         
+         
+         
+    """Create a new Debt"""
+    def Create_Debt(self, debtType: str, amout: int, interestRate: float, status: bool, debtDate: str = "DD/MM/YYYY HH:MM:SS", dueDate: str = "DD/MM/YYYY HH:MM:SS"):
+        if time == "DD/MM/YYYY HH:MM:SS":
+            time = self.__GetPresentTime()
+            
+        newDebt = Debt(self.__CreateID(), debtType, amout, interestRate, status, debtDate, dueDate)
+        
+    
+    
+    """Delete via ID"""
+    def Delete(self, ID: int) -> bool:
+        for transaction in self.__transactionList:
+            if transaction.Get_ID() == ID:
+                self.__transactionList.remove(transaction)
+                self.__balance += amount
+                messagebox.showinfo("Notification","Delete Successfully!")
+                return True
+        
+        for transfer in self.__transferList:
+            if transfer.Get_ID() == ID:
+                self.__transferList.remove(transfer)
+                self.__balance += amount
+                messagebox.showinfo("Notification","Delete Successfully!")
+                return True
+            
+        for debt in self.__DebtList:
+            if debt.Get_ID() == ID:
+                self.__DebtList.remove(debt)
+                self.__balance += amount
+                messagebox.showinfo("Notification","Delete Successfully!")
+                True
+                
+        return False
+                
+        
+    
+    """Update via ID"""
+    def Update(self, ID: int) -> bool:
+        for transaction in self.__transactionList:
+            if transaction.Get_ID() == ID:
+                transaction.Edit_Transaction(transactionType, amount, time, note, catalog)
+                self.__balance += amount
+                messagebox.showinfo("Notification","Update Successfully!")
+                return True
+            
+        for transfer in self.__transferList:
+            if transfer.Get_ID() == ID:
+                transfer.Edit_Transfer("transactionType, amount, time, note, catalog")
+                self.__balance += amount
+                messagebox.showinfo("Notification","Update Successfully!")
+                return True
+        
+        for transaction in self.__transactionList:
+            if transaction.Get_ID() == ID:
+                transaction.Edit_Debt("transactionType, amount, time, note, catalog")
+                self.__balance += amount
+                messagebox.showinfo("Notification","Update Successfully!")
+                return True
+        
+        return False
+                
+                
                 
     """Get the current balance in the account"""    
     def Get_Balance(self):
         return self.__balance
-    
-     
-    """View the history of transaction types (income/spending) made"""
-    def Show_Transaction_History(self, dateTime):
-        for transaction in self.__transactionList:
-            if transaction.Get_Time() == dateTime:
-                transaction.Get_Transaction_History()
+                 
     
     
     """Filter the list of catalogs"""
@@ -86,21 +137,5 @@ class Account:
             if newCatalog == transaction.Get_New_Catalog():
                 print(transaction)
         
-        
-    # def Create_Loan(self, lenderName, amount, loanTime, dueTime, interestRate, note, paymentStatus):
-    #     loan = Loan(lenderName, amount, loanTime, dueTime, interestRate, note, paymentStatus)
-    #     self.loanList.append(loan)
-        
-    
-    # def Delete_Loan(self, lenderName):
-    #     for loan in self.loanList[:]:
-    #         if loan.Get_Payment_Status() == 1 | lenderName == loan.Get_Lender_Name():
-    #             self.loanList.remove(loan)
-                
-                
-    # def Update_Loan(self, lenderName):
-    #     for loan in self.loanList:
-    #         if loan.Get_Lender_Name() == lenderName:
-    #             loan.Edit_Loan()
                 
             
