293f4686341da87dc80b6111090005ce67414940
Tran Trieu Duong <trantrieuduong2411@gmail.com>
2024-12-03 11:36:13 +0700

    Add files via upload

---
+++ GetPresentTime.py
@@ -0,0 +1,8 @@
+import datetime
+
+def GetPresentTime() -> str:
+    datetime_now = datetime.datetime.now()
+    formatted_datetime = datetime_now.strftime("%Y-%m-%d")
+    return formatted_datetime
+
+print(GetPresentTime())
