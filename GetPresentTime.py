da1d26aa736b4f0c751827b82c53757798e57ef9
Tran Trieu Duong <trantrieuduong2411@gmail.com>
2024-12-03 18:06:40 +0700

    Delete GetPresentTime.py

--- GetPresentTime.py
+++
@@ -1,6 +0,0 @@
-import datetime
-
-def GetPresentTime() -> str:
-    datetime_now = datetime.datetime.now()
-    formatted_datetime = datetime_now.strftime("%Y-%m-%d")
-    return formatted_datetime
