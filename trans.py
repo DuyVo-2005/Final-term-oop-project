068e64792108f96edad56fb1aa0142e2c8cf5a8b
Tran Trieu Duong <trantrieuduong2411@gmail.com>
2024-12-03 18:10:57 +0700

    Update trans.py

--- trans.py
+++ trans.py
@@ -1,192 +1,55 @@
-from tkinter import *
-from tkinter import messagebox
-import datetime
-import random
-from account import Account
-#from account import *
-
-catalog_hint = {
-    1: "Food",
-    2: "Moving",
-    3: "Entertainment",
-    4: "Salary",
-    5: "Portfolio",
-    6: "Other",
-}
-other_index = int(6)
+import account
+from GetPresentTime import GetPresentTime
 
 class Trans():
-    def __init__(self, fluctuation: str, amount: int, time: str) -> None:
-        self.__ID = ""
+    def __init__(self, ID: int, amount: int, time: str = "YYYY-MM-DD", note: str = "") -> None:
         self.__amount = amount
+        if time == "YYYY-MM-DD":
+            time = GetPresentTime()
         self.__time = time
+        self.__note = note
+        self.__ID = ID
 
-    def Get_ID(self):
+    def Get_ID(self) -> int:
         return self.__ID
-
-    def Set_ID(self, newID):
-        self.__ID =  newID
     
-    def Get_Amount(self):
+    def Get_Amount(self) -> int :
         return self.__amount
 
-    def Set_Amount(self, newAmount: int):
-        self.__amount = newAmount
-
-    def Get_Time(self):
+    def Get_Time(self) -> str:
         return self.__time
-
-    def Set_Time(self, newTime: str):
-        self.__time = newTime
     
-    def __GetPresentTime(
-) -> str:
-        datetime_now = datetime.datetime.now()
-        formatted_datetime = datetime_now.strftime("%Y-%m-%d")
-        return formatted_datetime
-
-
-class Transaction(Trans):
-    def __init__(self, fluctuation: str, amount: int, time: str, transType: str, note: str, catalog: str) -> None:
-        super().__init__(fluctuation, amount, time)
-        self.__transType = transType
-        self.__note = note
-        self.__catalog = catalog
-
-    def Get_Type(self):
-        return self.__transType
-
-    def Set_Type(self, newType: str = ""):
-         self.__transType = newType
-
-    def Get_Note(self):
+    def Get_Note(self) -> str:
         return self.__note
-
+    
     def Set_Note(self, newNote: str = ""):
         self.__note = newNote
 
-    def Get_Catalog(self):
-        return self.__catalog
+    def Set_Time(self, newTime: str):
+        self.__time = newTime
 
-    def Set_Catalog(self, newCatalog: str):
-        self.__catalog = newCatalog
 
-    def Edit_Transaction(self, transType, amount, time, note, catalog) -> bool:
-        """Function to edit existed transaction
-        Parameters:
-            transactionID(str): The id of transaction want to edit
-            transType(str): The type of transaction want to edit (income or spending)
-            amount(int): The amount of transaction want to edit
-            time(datetime): The time of transaction want to edit
-            note(str): The note of transaction want to edit
-            catalog(str): The catalog of transaction want to edit
-        Returns:
-            None
-        """
-        if (
-            transType.capitalize() != "income"
-            and transType.capitalize() != "spending"
-        ):
-            messagebox.showerror(
-                "Notification", "Transaction type must be income or spending!"
-            )
-            return False
-        else:
-            self.Set_Type(transType)
+class Transaction(Trans):
+    def __init__(self, cataLog: str, ID: int, fluctuation: str, amount: int, time: str = "YYYY-MM-DD", note: str = "") -> None:
+        super().__init__(ID, amount, time, note)
+        self.__cataLog = cataLog
+        self.__fluctuation = fluctuation
 
-        try:
-            if transType.capitalize() != "income":
-                self.Set_Amount(int(amount))
-            else:
-                #transaction type is spending
-                self.Set_Amount(int((-1) * amount))
-        except ValueError:
-            messagebox.showerror("Notification", "amount must be integer")
-            return False
+    def Get_Catalog(self) -> str:
+        return self.__cataLog
+    
+    def Get_Fluctuation(self) -> str:
+        return self.__fluctuation
 
-        # true format for time: DD/MM/YYYY
-        try:
-            dateFormat = "%d/%m/%Y"
-            self.Set_Time(datetime.datetime.strptime(time, dateFormat))
-        except ValueError:
-            messagebox.showerror(
-                "Notification", "Date time must be in format DD/MM/YYYY!"
-            )
-            return False
-        self.Set_Note(note)
-        global catalog_hint
-        if catalog.capitalize() not in catalog_hint.values():
-            messagebox.showerror("Notification", "Error catalog input!")
-            return False
-        else:
-            self.Set_Catalog(catalog)
-
-    def __str__(self):
-        return f"{self.__transactionID} | {self.__time} | {self.__transType} | {self.__amount} | {self.__note} | {self.__catalog}"
 
 class Transfer(Trans):
-    def __init__(self, fluctuation: str, amount: int, time: str, account, accountSource: Account, accountDestination: Account) -> None:
-        super().__init__(fluctuation, amount, time)
-        self.__accountSource = accountSource
-        self.__accountDestination = accountDestination
-        
-    def Get_Account_Name_Source(self):
-        return self.__accountSource
+    def __init__(self, ID: int, amount: int, sourceAccount: account.Account, desAccount: account.Account, time: str = "YYYY-MM-DD", note: str = "") -> None:
+        super().__init__(ID, amount, time, note)
+        self.__sourceAccount = sourceAccount
+        self.__desAccount = desAccount
 
-    def Set_Account_Name_Source(self, newaccountSource: str):
-        self.__accountSource = newaccountSource
-
-    def Get_Account_Name_Destination(self):
-        return self.__accountDestination
-
-    def Set_Account_Name_Destination(self, newaccountDestination: str):
-        self.__accountDestination = newaccountDestination
-
-    def Make_Transfer(self, sourceAccount: Account, destinationAccount: Account, amount: int) -> bool:
-        """Function to make transfer from sourceAccount to destinationAccount with the amount of money is amount
-        Parameters:
-            sourceAccount (class): the source account class makes a money transfer
-            destinationAccount (class): the destination account class receives a money transfer
-            amount (int): the amount of money transfer
-        Returns:
-            bool
-        """
-        if sourceAccount.Get_Balnace() < amount:
-            messagebox.showerror(
-                "Notification", "You don't have enough money for transfer"
-            )
-            return False
-        else:
-            self.Set_ID(sourceAccount.__CreateID())
-            self.__accountSource = sourceAccount.Get_Account_Name()
-            self.__accountDestination = destinationAccount.Get_Account_Name()
-            sourceAccount.Set_Balnace(sourceAccount.Get_Balnace() - amount)
-            destinationAccount.Set_Balnace(destinationAccount.Get_Balnace() + amount)
-            self.__amount = amount
-            sourceAccount.Get_Transfer_List().append(self)
-            destinationAccount.Get_Transfer_List().append(self)
-        return True
-    #def Edit_Transfer(self, sourceAccount: 'Account', destinationAccount: 'Account', amount: int) -> bool:
-#        """Function to make transfer from sourceAccount to destinationAccount with the amount of money is amount
-#        Parameters:
-#            sourceAccount (class): the source account class makes a money transfer
-#            destinationAccount (class): the destination account class receives a money transfer
-#            amount (int): the amount of money transfer
-#        Returns:
-#            bool
-#        """
-#        if sourceAccount.Get_Balnace() < amount:
-#            messagebox.showerror(
-#                "Notification", "You don't have enough money for transfer"
-#            )
-#            return False
-#        else:
-#            self.Set_ID(sourceAccount.__CreateID())
-#            self.__accountSource = sourceAccount.Get_Account_Name()
-#            self.__accountDestination = destinationAccount.Get_Account_Name()
-#            sourceAccount.Set_Balnace(sourceAccount.Get_Balnace() - amount)
-#            destinationAccount.Set_Balnace(destinationAccount.Get_Balnace() + amount)
-#            self.__amount = amount
-#            sourceAccount.Get_Transfer_List().append(self)
-#            destinationAccount.Get_Transfer_List().append(self)
-#        return True
+    def Get_Source_Account(self) -> account.Account:
+        return self.__sourceAccount
+    
+    def Get_Des_Account(self) -> account.Account:
+        return self.__desAccount
