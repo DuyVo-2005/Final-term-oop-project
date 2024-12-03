f9c355556904148d04496f789b5f456b5f576ac2
Tran Trieu Duong <trantrieuduong2411@gmail.com>
2024-12-03 11:36:48 +0700

    Update GetPresentTime.py

--- GetPresentTime.py
+++ GetPresentTime.py
@@ -4,5 +4,3 @@ def GetPresentTime() -> str:
     datetime_now = datetime.datetime.now()
     formatted_datetime = datetime_now.strftime("%Y-%m-%d")
     return formatted_datetime
-
-print(GetPresentTime())
