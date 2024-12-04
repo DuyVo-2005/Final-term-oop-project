df46efa71fc0a69840b91409822254068bc278a9
VanPhuHien <phien0689@gmail.com>
2024-12-04 11:50:11 +0700

    Update GetPresentTime.py

--- GetPresentTime.py
+++ GetPresentTime.py
@@ -3,4 +3,4 @@ import datetime
 def GetPresentTime() -> str:
     datetime_now = datetime.datetime.now()
     formatted_datetime = datetime_now.strftime("%Y-%m-%d")
-    return formatted_datetime
+    return formatted_datetime.strip()
