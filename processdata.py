d2a090e741e1724cfd0a703db50a5d4638048260
Tran Trieu Duong <trantrieuduong2411@gmail.com>
2024-12-04 13:12:07 +0700

    Update processdata.py

--- processdata.py
+++ processdata.py
@@ -6,7 +6,8 @@ from user import *
 AccountListPath = "data\\AccountList.txt"
 TransactionDataPath = "data\\TransactionData.csv"
 TransferDataPath = "data\\TransferData.csv"
-DebtDataPath = "data\\DebtList.txt"
+DebtListPath = "data\\DebtList.txt"
+DebtPaymentDataPath = "data\\DebtPaymentData.csv"
 
 def readData() -> User:
     user = User()
@@ -27,12 +28,7 @@ def readData() -> User:
         account_name = row._1.strip().lower()
         for account in list_account:
             if account.Get_Account_Name() == account_name:
-                user.Get_Account_By_Name(account_name).Create_Transaction(cataLog=str(row[6]).split("|")[0],
-                                                                          fluctuation=str(row[3]).strip().lower(),
-                                                                          amount=int(row[5]),
-                                                                          time=str(row[4]),
-                                                                          note=str(row[6]).split("|")[1])
-                user.Get_Account_By_Name(account_name).Edit_Transaction(ID=row[2],
+                user.Get_Account_By_Name(account_name).Create_Transaction(ID=row[2],
                                                                         cataLog=str(row[6]).split("|")[0],
                                                                         fluctuation=str(row[3]).strip().lower(),
                                                                         amount=int(row[5]),
@@ -49,13 +45,7 @@ def readData() -> User:
         amount = int(row[2])
         time = str(row[5])
         note = str(row[6])
-
-        user.Create_Transfer(amount=amount, 
-                             sourceAccountName=sourceAccName,
-                             desAccountName=desAccName,
-                             time=time,
-                             note=note)
-        user.Edit_Transfer(ID=id,
+        user.Create_Transfer(ID=id,
                            amount=amount, 
                            sourceAccountName=sourceAccName,
                            desAccountName=desAccName,
@@ -64,17 +54,26 @@ def readData() -> User:
 
 
     # Xử lý các Debt của User
-    data = open(DebtDataPath, 'r', encoding='utf-8').readlines()
+    data = open(DebtListPath, 'r', encoding='utf-8').readlines()
     for row in data:
-        user.Create_Debt(fluctuation= row.split("|")[1].strip().lower(), 
-                         amount= int(row.split("|")[2]), 
-                         dueDate= row.split("|")[4].strip(), 
-                         interestRate= float(row.split("|")[3]), 
-                         time= row.split("|")[5].strip(),  
-                         note= row.split("|")[6].strip())
-        
-        user.Edt
+        # ID|fluctuation|amount|interestRate|dueDate|time|note
+        user.Create_Debt(ID= int(row.split("|")[0]),
+                       fluctuation= row.split("|")[1].strip().lower(), 
+                       amount= int(row.split("|")[2]), 
+                       dueDate= row.split("|")[4].strip(), 
+                       interestRate= float(row.split("|")[3]), 
+                       time= row.split("|")[5].strip(),  
+                       note= row.split("|")[6].strip())
 
+    # Xử lý thông tin các debt
+    data = pd.read_csv(DebtPaymentDataPath, header=0, encoding='utf-8')
+    for row in data.itertuples():
+        id = int(row[1])
+        amount = int(row[2])
+        payDate = str(row[3]).strip()
+
+        user.Get_Debt_By_ID(ID=id).Add_Payment(amount=amount, paymentDate=payDate)
+        
     return user
 
             
@@ -141,3 +140,25 @@ def writeData(user: User):
     data.to_csv(TransferDataPath, mode='a', index=False, header=False, encoding='utf-8')
 
     # Xử lý Debt
+    columns = ['ID', 'Amount', 'PaymentDate']
+    data = pd.DataFrame(columns=columns)
+    data.to_csv(DebtPaymentDataPath, mode='w', index=False, encoding='utf-8')
+    debtlist = open(DebtListPath, 'w', encoding='utf-8')
+    for debt in user.Get_Debt_List():
+        # ID|fluctuation|amount|interestRate|dueDate|time|note
+        debtlist.write(f"{debt.Get_ID()}|{debt.Get_Fluctuation()}|{debt.Get_Amount()}|{float(debt.Get_InterestRate())}|{debt.Get_DueDate()}|{debt.Get_Time()}|{debt.Get_Note()}\n")
+
+        tid = []
+        amount = []
+        paymentDate = []
+        for payment in debt.Get_PaymentHistory():
+            tid.append(debt.Get_ID())
+            amount.append(payment['amount'])
+            paymentDate.append(payment['date'])
+
+        data = pd.DataFrame({
+            'ID': tid,
+            'Amount': amount,
+            'PaymentDate': paymentDate
+        })
+        data.to_csv(DebtPaymentDataPath, mode='a', index=False, header=False, encoding='utf-8')
