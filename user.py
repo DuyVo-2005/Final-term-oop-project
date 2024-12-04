ed203230ea5fd6c44514670a98eaca5e1f767eb4
VanPhuHien <phien0689@gmail.com>
2024-12-04 14:10:30 +0700

    Update user.py

--- user.py
+++ user.py
@@ -87,14 +87,17 @@ class User:
             return True
         return False
 
-    def Create_Transfer(self, amount: int, sourceAccountName: str, desAccountName: str, time: str = "YYYY-MM-DD", note: str = "", ID: int = 0):
-        sourceAccount = self.Get_Account_By_Name(sourceAccountName.strip().lower())
-        desAccount = self.Get_Account_By_Name(desAccountName.strip().lower())
-
-        newTransfer = Transfer(ID=self.__CreateID() if ID == 0 else ID, amount=amount, sourceAccount=sourceAccount, desAccount=desAccount, note=note.strip(), time=time.strip())
-        sourceAccount.Set_Balance(sourceAccount.Get_Balance() - amount, True)
-        desAccount.Set_Balance(desAccount.Get_Balance() + amount, True)
-        self.__transferList.append(newTransfer)
+    def Create_Transfer(self, amount: int, sourceAccountName: str, desAccountName: str, time: str = "YYYY-MM-DD", note: str = "", ID: int = 0) -> bool:
+        if amount > 0:
+            sourceAccount = self.Get_Account_By_Name(sourceAccountName.strip().lower())
+            desAccount = self.Get_Account_By_Name(desAccountName.strip().lower())
+    
+            newTransfer = Transfer(ID=self.__CreateID() if ID == 0 else ID, amount=amount, sourceAccount=sourceAccount, desAccount=desAccount, note=note.strip(), time=time.strip())
+            sourceAccount.Set_Balance(sourceAccount.Get_Balance() - amount, True)
+            desAccount.Set_Balance(desAccount.Get_Balance() + amount, True)
+            self.__transferList.append(newTransfer)
+            return True
+        return False
 
     def Delete_Transfer(self, ID: int) -> bool:
         trans = self.Get_Transfer_By_ID(ID)
@@ -121,9 +124,12 @@ class User:
             return True
         return False
     
-    def Create_Debt(self, fluctuation: str, amount: int, dueDate: str, interestRate: float = 0.0, time: str = "YYYY-MM-DD",  note: str = "", ID: int = 0):
-        newDebt = Debt(ID=self.__CreateID() if ID == 0 else ID, fluctuation=fluctuation, amount=amount, interestRate=interestRate, time=time.strip(), dueDate=dueDate.strip(), note=note.strip())
-        self.__debtList.append(newDebt)
+    def Create_Debt(self, fluctuation: str, amount: int, dueDate: str, interestRate: float = 0.0, time: str = "YYYY-MM-DD",  note: str = "", ID: int = 0) -> bool:
+        if amount > 0:
+            newDebt = Debt(ID=self.__CreateID() if ID == 0 else ID, fluctuation=fluctuation, amount=amount, interestRate=interestRate, time=time.strip(), dueDate=dueDate.strip(), note=note.strip())
+            self.__debtList.append(newDebt)
+            return True
+        return False
         
     def Update_Debt(self, ID: int, amount: int, paymentDate: str = "YYYY-MM-DD") -> bool:
         debt = self.Get_Debt_By_ID(ID)
